ce92c6f064d91abacaf4bacf3e28d252f3b1cfc0
Windead <59016149+WindeadGhost@users.noreply.github.com>
2023-07-13 13:38:43 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using NoteNet.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoteNet
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            double height = SystemParameters.PrimaryScreenHeight;
+            double width = SystemParameters.PrimaryScreenWidth;
+
+            double workAreaHeight = SystemParameters.WorkArea.Height;
+            double workAreaWidth = SystemParameters.WorkArea.Width;
+
+            double heightDiff = height - workAreaHeight;
+            double widthDiff = width - workAreaWidth;
+
+            double widthRatio = 0.25;
+            double heightRatio = 0.6;
+
+            int offset = 10;
+
+            this.Left = width - (width * widthRatio) - widthDiff - offset;
+            this.Top = height - (height * heightRatio) - heightDiff - offset;
+
+            Console.WriteLine(height + " " + width);
+            Console.WriteLine(workAreaHeight + " " + workAreaWidth);
+            this.MaxWidth = width * widthRatio;
+            this.MinWidth = width * widthRatio;
+            this.Width = width * widthRatio;
+            this.MaxHeight = height * heightRatio;
+            this.MinHeight = height * heightRatio;
+            this.Height = height * heightRatio;
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            /*Console.WriteLine("Height : " + this.Height);
+            Console.WriteLine("Width : " + this.Width);
+            Console.WriteLine("Top : " + this.Top);
+            Console.WriteLine("Left : " + this.Left);*/
+        }
+
+        private void Main_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Console.WriteLine("Height : " + this.Height);
+            Console.WriteLine("Width : " + this.Width);
+            Console.WriteLine("Top : " + this.Top);
+            Console.WriteLine("Left : " + this.Left);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddNote AN = new AddNote(this.Width - 50, this.Height - 50, this.Left, this.Top);
+            AN.ShowDialog();
+        }
+    }
+}
---
+++ Windows/AddNote.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteNet.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour AddNote.xaml
+    /// </summary>
+    public partial class AddNote : Window
+    {
+        public AddNote(double width = 0, double height = 0, double left = 0, double top = 0)
+        {
+            this.Width = width;
+            this.MinWidth = width;
+            this.MaxWidth = width;
+            this.Height = height;
+            this.MinHeight = height;
+            this.MaxHeight = height;
+            this.Left = left + 25;
+            this.Top = top + 25;
+            InitializeComponent();
+            
+
+            Console.WriteLine("Height : " + this.Height);
+            Console.WriteLine("Width : " + this.Width);
+            Console.WriteLine("Top : " + this.Top);
+            Console.WriteLine("Left : " + this.Left);
+        }
+    }
+}
